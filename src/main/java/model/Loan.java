09b64502498d1b6549fbb987e52d4873decd27dc
evavdb1 <evavandenbroucke@gmail.com>
2025-09-02 15:29:18 +0200

    added

--- src/main/java/model/Loan.java
+++ src/main/java/model/Loan.java
@@ -4,44 +4,43 @@ import java.util.Objects;
 
 public class Loan {
 
-    private String loanID;
     private Date loanDate;
     private Date dueDate;
     private Date returnDate;
+    private boolean status;
     private Book book;
     private Member member;
 
-    public Loan(String loanID, Date loanDate, Date dueDate) {
-        this.loanID = loanID;
+    public Loan(Book book, Member member, Date loanDate, Date dueDate) {
+        this.book = book;
+        this.member = member;
         this.loanDate = loanDate;
         this.dueDate = dueDate;
     }
 
-    public String getLoanID() {        return loanID;    }
-    public void setLoanID(String loanID) {        this.loanID = loanID;    }
     public Date getLoanDate() {        return loanDate;    }
     public void setLoanDate(Date loanDate) {        this.loanDate = loanDate;    }
     public Date getDueDate() {        return dueDate;    }
     public void setDueDate(Date dueDate) {        this.dueDate = dueDate;    }
     public Date getReturnDate() {        return returnDate;    }
     public void setReturnDate(Date returnDate) {        this.returnDate = returnDate;    }
-
+    public boolean isStatus() {        return status;    }
+    public void setStatus(boolean status) {        this.status = status;    }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Loan loan = (Loan) o;
-        return Objects.equals(loanID, loan.loanID) && Objects.equals(loanDate, loan.loanDate) && Objects.equals(dueDate, loan.dueDate) && Objects.equals(returnDate, loan.returnDate);
+        return status == loan.status && Objects.equals(loanDate, loan.loanDate) && Objects.equals(dueDate, loan.dueDate) && Objects.equals(returnDate, loan.returnDate);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(loanID, loanDate, dueDate, returnDate);
+        return Objects.hash(loanDate, dueDate, returnDate, status);
     }
 
     @Override
     public String toString() {
-        return "Loan {loanID: "+loanID+", loanDate: "+loanDate+", dueDate: " + dueDate +
-                '}';
+        return "Loan {loanDate: "+loanDate+", dueDate: " + dueDate;
     }
 }
