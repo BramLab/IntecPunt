fb5a2d06e478e9c13006e5a2b76a9b6f41d6299e
max <nzalimaxime1@gmail.com>
2025-09-02 13:38:54 +0200

    modification sequence diagram , class member , MemberService, MemberRepository

---
+++ src/main/java/Member.java
@@ -0,0 +1,58 @@
+import java.util.Objects;
+
+public class Member {
+    private String name;
+    private int age;
+    private String memberId; // Unique ID "
+    private String email;
+
+    public Member(String name, int age, String email, String memberId) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.memberId = memberId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Member member)) return false;
+        return age == member.age && Objects.equals(name, member.name) && Objects.equals(memberId, member.memberId) && Objects.equals(email, member.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, memberId, email);
+    }
+}
