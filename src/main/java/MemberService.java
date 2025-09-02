bd2d09f5b64120697483c2802a7828b041b4a797
max <nzalimaxime1@gmail.com>
2025-09-02 14:05:00 +0200

    Service Member part

--- src/main/java/MemberService.java
+++ src/main/java/MemberService.java
@@ -1,3 +1,23 @@
+import java.util.List;
+
 public class MemberService {
+    private MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+        // adding a member
+    public void addMember(Member member) {
+        memberRepository.save(member);
+    }
+    // remove a member
+    public void removeMember(Long id) {
+        memberRepository.deleteById(id);
+    }
+    // Lister tous les membres
+    public List<Member> listMembers() {
+        return memberRepository.findAll();
+    }
+
 
 }
