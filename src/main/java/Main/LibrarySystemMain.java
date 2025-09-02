afe5e37d83d35285ecc1e270048755a482103d90
3 Klasses  Update <hajji.chaima@outlook.fr>
2025-09-02 13:53:39 +0200

    update

--- src/main/java/Main/LibrarySystemMain.java
+++ src/main/java/Main/LibrarySystemMain.java
@@ -1,15 +1,27 @@
 package Main;
 
+import repository.BookRepository;
+import service.BookService;
+import model.Book;
+
 public class LibrarySystemMain {
     public static void main(String[] args) {
         // Maak repositories
-        //BookRepository bookRepository = new BookRepository();
+        BookRepository bookRepository = new BookRepository();
 
         //Maak Services
-        //BookService bookService = new BookService(bookRepository);
+        BookService bookService = new BookService();
+
 
         // Voeg enkele boeken toe
-        //Boek b1 = new Book();
+        Book b1 = new Book("OCA java 8");
+        Book b2 = new Book("Harry Poter");
+        Book b3 = new Book("Malcom x");
+
+        bookService.addBook(b1);
+        bookService.addBook(b2);
+        bookService.addBook(b3);
+
 
     }
 }
