c737c0def1ed16bcb33e00acfd49bbaceca43101
Shayma1080 <hajji.chaima@outlook.fr>
2025-09-02 18:40:28 +0200

    update

--- src/main/java/Main/LibrarySystemMain.java
+++ src/main/java/Main/LibrarySystemMain.java
@@ -13,42 +13,30 @@ import java.util.Arrays;
 public class LibrarySystemMain {
     public static void main(String[] args) {
 
-        // Maak repositories
         BookRepository bookRepository = new BookRepository();
 
-
-        //Maak Services
         BookService bookService = new BookService();
-//
-//
-//        System.out.println("\nbookService.getBooks: " + Arrays.toString(bookService.getBooks().toArray()));
-//
-//        Book book = bookService.searchBook("De Hobbit", "John Ronald Reuel Tolkien", 2015);
-//        System.out.println("\nbookService.searchBook(title, author, yearPublished): " + book);
-//
-//        book = bookService.searchBook("9789067282451");
-//        System.out.println("\nbookService.searchBook(isbn): " + book);
-//
-//        System.out.println("\nbookService.countCopies: " + bookService.countCopies(book));
 
 
+        System.out.println("\nbookService.getBooks: " + Arrays.toString(bookService.getBooks().toArray()));
+
+        Book book = bookService.searchBook("De Hobbit", "John Ronald Reuel Tolkien", 2015);
+        System.out.println("\nbookService.searchBook(title, author, yearPublished): " + book);
+
+        book = bookService.searchBook("9789067282451");
+        System.out.println("\nbookService.searchBook(isbn): " + book);
+
+        System.out.println("\nbookService.countCopies: " + bookService.countCopies(book));
 
 
-        // Voeg enkele boeken toe
-        Book b1 = new Book("OCA java 8");
-        Book b2 = new Book("Harry Poter");
-        Book b3 = new Book("Malcom x");
-
-        bookService.addBook(b1);
-        bookService.addBook(b2);
-        bookService.addBook(b3);
 
         Member member1 = new Member("Hajar",24, "hajar.benwahib",1547854154L);
-        Loan loan = new Loan(123456L,member1,b1);
+        Loan loan = new Loan(123456L,member1,book);
         loan.setLoanDate(LocalDate.now());
-        loan.setDueDate(loan.getLoanDate());
+        loan.setDueDate(loan.getLoanDate().plusDays(14));
         loan.setReturnDate(LocalDate.now());
         loan.setStatus(LoanStatus.RETURNED);
+        System.out.println(loan);
 
 
 
