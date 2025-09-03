b9cdb97d5915efcc28e5750cd9aba97d3310d222
evavdb1 <evavandenbroucke@gmail.com>
2025-09-03 09:28:11 +0200

    added

--- src/main/java/model/Loan.java
+++ src/main/java/model/Loan.java
@@ -7,15 +7,16 @@ public class Loan {
     private Date loanDate;
     private Date dueDate;
     private Date returnDate;
-    private boolean status;
+    private LoanStatus status;
     private Book book;
     private Member member;
 
-    public Loan(Book book, Member member, Date loanDate, Date dueDate) {
-        this.book = book;
-        this.member = member;
+    public Loan(Date loanDate, Date dueDate, Book book, Member member) {
         this.loanDate = loanDate;
         this.dueDate = dueDate;
+        this.book = book;
+        this.member = member;
+        this.status = LoanStatus.ACTIVE;
     }
 
     public Date getLoanDate() {        return loanDate;    }
@@ -23,24 +24,45 @@ public class Loan {
     public Date getDueDate() {        return dueDate;    }
     public void setDueDate(Date dueDate) {        this.dueDate = dueDate;    }
     public Date getReturnDate() {        return returnDate;    }
-    public void setReturnDate(Date returnDate) {        this.returnDate = returnDate;    }
-    public boolean isStatus() {        return status;    }
-    public void setStatus(boolean status) {        this.status = status;    }
+    public LoanStatus getStatus() {        return status;    }
+    public void setStatus(LoanStatus status) {        this.status = status;    }
+    public Book getBook() {        return book;    }
+    public Member getMember() {        return member;    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+        this.status = LoanStatus.RETURNED;
+    }
+
+    public boolean isOverdue() {
+        return status == LoanStatus.ACTIVE && new Date().after(dueDate);
+    }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Loan loan = (Loan) o;
-        return status == loan.status && Objects.equals(loanDate, loan.loanDate) && Objects.equals(dueDate, loan.dueDate) && Objects.equals(returnDate, loan.returnDate);
+        return Objects.equals(loanDate, loan.loanDate) &&
+                Objects.equals(dueDate, loan.dueDate) &&
+                Objects.equals(returnDate, loan.returnDate) &&
+                Objects.equals(book, loan.book) &&
+                Objects.equals(member, loan.member);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(loanDate, dueDate, returnDate, status);
+        return Objects.hash(loanDate, dueDate, returnDate, status, book, member);
     }
 
     @Override
     public String toString() {
-        return "Loan {loanDate: "+loanDate+", dueDate: " + dueDate;
+        return "Loan{" +
+                "loanDate=" + loanDate +
+                ", dueDate=" + dueDate +
+                ", returnDate=" + returnDate +
+                ", status=" + status +
+                ", book=" + book +
+                ", member=" + member +
+                '}';
     }
 }
--- src/main/java/model/Member.java
+++ src/main/java/model/Member.java
@@ -15,37 +15,14 @@ public class Member {
         this.memberId = memberId;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(String memberId) {
-        this.memberId = memberId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public String getName() {        return name;    }
+    public void setName(String name) {        this.name = name;    }
+    public int getAge() {        return age;    }
+    public void setAge(int age) {        this.age = age;    }
+    public String getMemberId() {        return memberId;    }
+    public void setMemberId(String memberId) {        this.memberId = memberId;    }
+    public String getEmail() {        return email;    }
+    public void setEmail(String email) {        this.email = email;    }
 
     @Override
     public boolean equals(Object o) {
